c41165188d47586315fd6be8eab50818de27864b
Sune Rønne <SURN@nykredit.dk>
2025-02-06 16:11:49 +0100

    Starting off

---
+++ Skalmejen.UI/Components/Player/SpotifyPlayerComponent.razor.js
@@ -0,0 +1,26 @@
+﻿export function setup() {
+
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+    window.onSpotifyWebPlaybackSDKReady = () => {
+
+        const player = new window.Spotify.Player({
+            name: 'Web Playback SDK',
+            getOAuthToken: cb => { cb(props.token); },
+            volume: 0.5
+        });
+
+        setPlayer(player);
+
+        player.addListener('ready', ({ device_id }) => {
+            console.log('Ready with Device ID', device_id);
+        });
+
+        player.addListener('not_ready', ({ device_id }) => {
+            console.log('Device ID has gone offline', device_id);
+        });
+    player.connect();
+
+    };
+}
