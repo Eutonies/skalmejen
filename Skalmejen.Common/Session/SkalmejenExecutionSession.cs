5a32bab3c0bd3f8558b5f1c803cd9de8d5c3771e
Sune Rønne <SURN@nykredit.dk>
2025-02-13 20:45:41 +0100

    Perty cool

---
+++ Skalmejen.Common/Session/SkalmejenExecutionSession.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skalmejen.Common.Session;
+public record SkalmejenExecutionSession
+{
+}
---
+++ Skalmejen.Mobile/Components/WelcomeComponent.razor.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Skalmejen.Mobile.Components;
+
+public partial class WelcomeComponent
+{
+
+
+    private string _joinText = "";
+    public string JoinText { 
+        get => _joinText;
+        set
+        {
+            _joinText = value ?? "";
+            VerifyJoinCode();
+            InvokeAsync(StateHasChanged);
+        }
+    }
+
+
+    private string _userName = "";
+    public string UserName
+    {
+        get => _userName;
+        set
+        {
+            _userName = value ?? "";
+            VerifyUserName();
+            InvokeAsync(StateHasChanged);
+        }
+    }
+
+    private bool _joinCodeIsOk = false;
+    private bool _userNameIsOk = false;
+    private void VerifyJoinCode()
+    {
+        _joinCodeIsOk = _joinText.Length > 5;
+    }
+
+    private void VerifyUserName()
+    {
+        _userNameIsOk = _userName.Length > 2;
+    }
+
+    private bool DisableJoinButton => !(_joinCodeIsOk && _userNameIsOk);
+
+}
