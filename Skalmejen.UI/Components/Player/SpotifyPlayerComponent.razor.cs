a3dcae4bff41fdf30c4a4e9097f240949bbc4256
Sune Rønne <SURN@nykredit.dk>
2025-02-15 00:57:27 +0100

    Alright

--- Skalmejen.UI/Components/Player/SpotifyPlayerComponent.razor.cs
+++ Skalmejen.UI/Components/Player/SpotifyPlayerComponent.razor.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Options;
 using Microsoft.JSInterop;
-using Skalmejen.UI.Configuration;
+using Skalmejen.Common.Music.Model;
+using Skalmejen.Integration.Configuration;
 
 namespace Skalmejen.UI.Components.Player;
 
@@ -25,15 +26,42 @@ public partial class SpotifyPlayerComponent
         if (!_didInit)
         {
             var token = Conf.Value.DeveloperToken;
-            await InitatePlayer(token);
+            //await InitatePlayer(token);
             _didInit = true;
         }
     }
+    private bool _isPlaying = false;
+    private bool IsPlaying => _isPlaying;
+
+    private SpotifyTrack? _currentTrack = new SpotifyTrack(
+        TrackId: "0tGPJ0bkWOUmH7MEOR77qc",
+        Name: "Cut To The Feeling",
+        Artist: "Carly Rae Jepsen",
+        Images: [
+            new SpotifyImage("https://i.scdn.co/image/ab67616d00001e027359994525d219f64872d3b1", 300, 300)
+            ]
+        );
 
     private async Task InitatePlayer(string token)
     {
         var module = await JS.InvokeAsync<IJSObjectReference>("import", "./Components/Player/SpotifyPlayerComponent.razor.js");
         await module.InvokeVoidAsync("SkalmejenSpotifyClient.setup", token);
+    }
+
+    private void OnPlayPauseClicked()
+    {
+        _isPlaying = !_isPlaying;
+        InvokeAsync(StateHasChanged);
+    }
+
+    private void OnBackwardClicked()
+    {
+
+    }
+
+    private void OnForwardClicked()
+    {
+
     }
 
 }
